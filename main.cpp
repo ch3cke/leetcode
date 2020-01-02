248bffad34cd19847fe7d59ab6c254aa6900cf77
ch3cke <ch3cke@gmail.com>
2020-01-02 23:49:24 +0800

    changes 13

--- main.cpp
+++ main.cpp
@@ -6,27 +6,71 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int max =0;
-        int k = 0;
-        for(int i=0;i<s.size();i++){
-            for(int j = k;j<i;j++){
-                if(s[i]==s[j]){
-                    k=j+1;
-                    break;
+    int romanToInt(string s) {
+        int result = 0;
+        for(int i =0;i<s.size();i++){
+            if(s[i]=='I'){
+                if(s[i+1]=='V'){
+                    result = result+4;
+                    i++;
+                } else if(s[i+1]=='X'){
+                    result = result+9;
+                    i++;
+                } else{
+                    result = result+1;
                 }
+                continue;
             }
-            if(i-k+1>max){
-                max = i-k+1;
+            if(s[i]=='X'){
+                if(s[i+1]=='L'){
+                    result = result+40;
+                    i++;
+                } else if(s[i+1]=='C'){
+                    result = result+90;
+                    i++;
+                } else{
+                    result = result+10;
+                }
+                continue;
+            }
+            if(s[i]=='C'){
+                if(s[i+1]=='D'){
+                    result = result+400;
+                    i++;
+                } else if(s[i+1]=='M'){
+                    result = result+900;
+                    i++;
+                } else{
+                    result = result+100;
+                }
+                continue;
+            }
+            switch (s[i]){
+                case 'I':
+                    result = result+ 1;
+                    break;
+                case 'V': result = result+ 5;
+                    break;
+                case 'X': result = result+ 10;
+                    break;
+                case 'L': result = result+ 50;
+                    break;
+                case 'C': result = result+ 100;
+                    break;
+                case 'D': result = result+ 500;
+                    break;
+                case 'M': result = result+ 1000;
+                    break;
+
             }
         }
-        return max;
+        return result;
     }
 };
 
 int main() {
-    string s = "aab";
+    string s = "MCMXCIV";
     Solution test;
-    cout<<test.lengthOfLongestSubstring(s);
+    cout<<test.romanToInt(s);
     return 0;
 }
